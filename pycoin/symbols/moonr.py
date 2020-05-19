0433205071ea5771594e8e7d40f0112fef92a87c
mebagger <33333802+mebagger@users.noreply.github.com>
2020-05-18 18:33:30 -0700

    mooncoin updates

--- pycoin/symbols/moonr.py
+++ pycoin/symbols/moonr.py
@@ -3,11 +3,8 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 
 network = create_bitcoinish_network(
     symbol="MOONR", network_name="Mooncoin", subnet_name="regtest",
-    wif_prefix_hex="EE", address_prefix_hex="112", pay_to_script_prefix_hex="39",
+    wif_prefix_hex="EE", address_prefix_hex="70", pay_to_script_prefix_hex="C3",
     bip32_prv_prefix_hex="04358394", bip32_pub_prefix_hex="043587CF", bech32_hrp="moonr",
     magic_header_hex="f5d6c4f7", default_port=13664 ,
 	)
     
-	
-	
-	
--- pycoin/symbols/moont.py
+++ pycoin/symbols/moont.py
@@ -3,7 +3,7 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 
 network = create_bitcoinish_network(
     symbol="MOONT", network_name="Mooncoin", subnet_name="test",
-    wif_prefix_hex="F1",  address_prefix_hex="113", pay_to_script_prefix_hex="32",
+    wif_prefix_hex="F1",  address_prefix_hex="71", pay_to_script_prefix_hex="C4",
     bip32_prv_prefix_hex="04358394", bip32_pub_prefix_hex="043587CF", bech32_hrp="moont",
     magic_header_hex="f3d2c8f1", default_port=14664,
     dns_bootstrap=["testnet-seed01.moonypool.com"],
