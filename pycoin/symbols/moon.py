8b11c17919b27ea18b9ee813ba139d6883f67a84
mebagger <33333802+mebagger@users.noreply.github.com>
2020-05-18 17:54:56 -0700

    moon regtest update

--- pycoin/symbols/moon.py
+++ pycoin/symbols/moon.py
@@ -2,10 +2,10 @@ from pycoin.networks.bitcoinish import create_bitcoinish_network
 
 
 network = create_bitcoinish_network(
-    network_name="Mooncoin", symbol="MOON", subnet_name="main",
-    wif_prefix_hex="83", sec_prefix="MOONSEC:", address_prefix_hex="3", pay_to_script_prefix_hex="32",
+    symbol="MOON", network_name="Mooncoin", subnet_name="main",
+    wif_prefix_hex="83", address_prefix_hex="3", pay_to_script_prefix_hex="32",
     bip32_prv_prefix_hex="0488ADE4", bip32_pub_prefix_hex="0488B21E", bech32_hrp="moon",
-    magic_header_hex="f9f7c0e8", default_port=44664,
+    magic_header_hex="f9f7c0e8", default_port="44664" ,
     dns_bootstrap=["dnsseed01.moonypool.com", "dnsseed02.moonypool.com", "dnsseed03.moonypool.com",])
 	
 	
