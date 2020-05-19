a87365499ed0f554efaba84477e48aebef936217
mebagger <33333802+mebagger@users.noreply.github.com>
2020-05-18 17:44:50 -0700

    moon seed updates

--- pycoin/symbols/moon.py
+++ pycoin/symbols/moon.py
@@ -6,6 +6,6 @@ network = create_bitcoinish_network(
     wif_prefix_hex="83", sec_prefix="MOONSEC:", address_prefix_hex="3", pay_to_script_prefix_hex="32",
     bip32_prv_prefix_hex="0488ADE4", bip32_pub_prefix_hex="0488B21E", bech32_hrp="moon",
     magic_header_hex="f9f7c0e8", default_port=44664,
-    dns_bootstrap=["dnsseed01.moonypool.com"])
+    dns_bootstrap=["dnsseed01.moonypool.com", "dnsseed02.moonypool.com", "dnsseed03.moonypool.com",])
 	
 	
